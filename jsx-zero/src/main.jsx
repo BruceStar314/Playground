7ec10d4e00e8ac88b6964a369706dd4b4ecaeb23
Bruce <js.python03@gmail.com>
2025-11-13 12:06:04 -0800

    Bruh I dont even know

--- jsx-zero/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client";
-import "./CSS/index.css";
-import Structure from "./App.jsx";
-  
-createRoot(document.getElementById("root")).render(
-  <>
-    <Structure  />
-  </>
-)
